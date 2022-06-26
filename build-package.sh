2604f70c8854a051a6e1dc5b708484f497dc8577
Yong He <yhe@nvidia.com>
2022-06-25 17:43:27 -0700

    Update workflow.

--- build-package.sh
+++ build-package.sh
@@ -2,21 +2,21 @@ npm install
 npm install -g vsce
 
 ls ./
-mkdir -p ./bin
+mkdir -p ./tmp
 echo "extracting $WIN32ZIP"
-unzip -n $WIN32ZIP -d ./bin
+unzip -n $WIN32ZIP -d ./tmp
 echo "extracting $LINUX64ZIP"
-unzip -n $LINUX64ZIP -d ./bin
+unzip -n $LINUX64ZIP -d ./tmp
 
 mkdir -p ./server/bin/win-x32
-cp ./bin/bin/windows-x86/release/slang.dll ./server/bin/win-x32/slang.dll
-cp ./bin/bin/windows-x86/release/slangd.exe ./server/bin/win-x32/slangd.exe
+cp ./tmp/bin/windows-x86/release/slang.dll ./server/bin/win-x32/slang.dll
+cp ./tmp/bin/windows-x86/release/slangd.exe ./server/bin/win-x32/slangd.exe
 vsce package --target win32-ia32
 
 rm -rf ./server/bin/
 mkdir -p ./server/bin/linux-x64
-cp ./bin/bin/linux-x64/release/libslang.so ./server/bin/linux-x64/libslang.so
-cp ./bin/bin/linux-x64/release/slangd ./server/bin/linux-x64/slangd
+cp ./tmp/bin/linux-x64/release/libslang.so ./server/bin/linux-x64/libslang.so
+cp ./tmp/bin/linux-x64/release/slangd ./server/bin/linux-x64/slangd
 chmod +x ./server/bin/linux-x64/slangd
 vsce package --target linux-x64
 
