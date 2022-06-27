270faa0961d2f9a36de574c27ee4b18e6713070d
Yong He <yhe@nvidia.com>
2022-06-27 00:34:09 -0700

    Update release workflow.

--- build-package.sh
+++ build-package.sh
@@ -7,6 +7,8 @@ echo "extracting $WIN32ZIP"
 unzip -n $WIN32ZIP -d ./tmp
 echo "extracting $LINUX64ZIP"
 unzip -n $LINUX64ZIP -d ./tmp
+echo "extracting $MACOSX64ZIP"
+unzip -n $MACOSX64ZIP -d ./tmp
 
 mkdir -p ./server/bin/win-x32
 cp ./tmp/bin/windows-x86/release/slang.dll ./server/bin/win-x32/slang.dll
@@ -22,6 +24,7 @@ vsce package --target linux-x64
 
 rm -rf ./server/bin/
 mkdir -p ./server/bin/darwin
-unzip slang-macos-dist.zip -d ./server/bin/darwin/
+cp ./tmp/bin/macosx-x64/release/libslang.dylib ./server/bin/darwin/libslang.dylib
+cp ./tmp/bin/macosx-x64/release/slangd ./server/bin/darwin/slangd
 chmod +x ./server/bin/darwin/slangd
 vsce package --target darwin-x64
