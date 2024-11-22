8e74e1f2f57871d4e0958066a7e5fa922c797ab8
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2024-11-22 00:06:06 -0300

    fix: modify build script for server path resolve

--- build-package.sh
+++ build-package.sh
@@ -3,27 +3,27 @@ npm install -g @vscode/vsce
 
 ls ./
 mkdir -p ./tmp
-echo "extracting $WIN32ZIP"
-unzip -n $WIN32ZIP -d ./tmp
-mkdir -p ./server/bin/win-x64
-cp ./tmp/bin/slang.dll ./server/bin/win-x64/slang.dll
-cp ./tmp/bin/slangd.exe ./server/bin/win-x64/slangd.exe
+echo "extracting $WIN32_X64_ZIP"
+unzip -n $WIN32_X64_ZIP -d ./tmp
+mkdir -p ./server/bin/win32-x64
+cp ./tmp/bin/slang.dll ./server/bin/win32-x64/slang.dll
+cp ./tmp/bin/slangd.exe ./server/bin/win32-x64/slangd.exe
 vsce package --target win32-x64
 rm -rf ./tmp
 
 mkdir -p ./tmp
-echo "extracting $WINARMZIP"
-unzip -n $WINARMZIP -d ./tmp
+echo "extracting $WIN32_ARM64_ZIP"
+unzip -n $WIN32_ARM64_ZIP -d ./tmp
 rm -rf ./server/bin/
-mkdir -p ./server/bin/win-arm64
-cp ./tmp/bin/slang.dll ./server/bin/win-arm64/slang.dll
-cp ./tmp/bin/slangd.exe ./server/bin/win-arm64/slangd.exe
+mkdir -p ./server/bin/win32-arm64
+cp ./tmp/bin/slang.dll ./server/bin/win32-arm64/slang.dll
+cp ./tmp/bin/slangd.exe ./server/bin/win32-arm64/slangd.exe
 vsce package --target win32-arm64
 rm -rf ./tmp
 
 mkdir -p ./tmp
-echo "extracting $LINUX64ZIP"
-unzip -n $LINUX64ZIP -d ./tmp
+echo "extracting $LINUX_X64_ZIP"
+unzip -n $LINUX_X64_ZIP -d ./tmp
 rm -rf ./server/bin/
 mkdir -p ./server/bin/linux-x64
 cp ./tmp/lib/libslang.so ./server/bin/linux-x64/libslang.so
@@ -33,8 +33,19 @@ vsce package --target linux-x64
 rm -rf ./tmp
 
 mkdir -p ./tmp
-echo "extracting $MACOSX64ZIP"
-unzip -n $MACOSX64ZIP -d ./tmp
+echo "extracting $LINUX_ARM64_ZIP"
+unzip -n $LINUX_ARM64_ZIP -d ./tmp
+rm -rf ./server/bin/
+mkdir -p ./server/bin/linux-arm64
+cp ./tmp/lib/libslang.so ./server/bin/linux-arm64/libslang.so
+cp ./tmp/bin/slangd ./server/bin/linux-arm64/slangd
+chmod +x ./server/bin/linux-arm64/slangd
+vsce package --target linux-arm64
+rm -rf ./tmp
+
+mkdir -p ./tmp
+echo "extracting $DARWIN_X64_ZIP"
+unzip -n $DARWIN_X64_ZIP -d ./tmp
 rm -rf ./server/bin/
 mkdir -p ./server/bin/darwin-x64
 cp ./tmp/lib/libslang.dylib ./server/bin/darwin-x64/libslang.dylib
@@ -44,8 +55,8 @@ vsce package --target darwin-x64
 rm -rf ./tmp
 
 mkdir -p ./tmp
-echo "extracting $MACOSAARCH64ZIP"
-unzip -n $MACOSAARCH64ZIP -d ./tmp
+echo "extracting $DARWIN_ARM64_ZIP"
+unzip -n $DARWIN_ARM64_ZIP -d ./tmp
 rm -rf ./server/bin/
 mkdir -p ./server/bin/darwin-arm64
 cp ./tmp/lib/libslang.dylib ./server/bin/darwin-arm64/libslang.dylib
