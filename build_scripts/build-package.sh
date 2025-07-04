1912eaedb093882399cf9d3ab722efacefa7d6e5
Devon7925 <devonrutledge03@gmail.com>
2025-07-04 14:39:09 -0600

    Optimize build sizes

--- build_scripts/build-package.sh
+++ build_scripts/build-package.sh
@@ -28,6 +28,13 @@ target_build() {
   rm -rf ./server/bin/
 }
 
+echo "updating .vscodeignore for native builds"
+cp .vscodeignore .vscodeignore.bak
+# Exclude web files for native builds
+echo 'media/*.worker.js' >> .vscodeignore
+echo 'client/dist/browserClientMain.js' >> .vscodeignore
+echo 'server/dist/browserServerMain.js' >> .vscodeignore
+
 target_build "$WIN32_X64_ZIP" win32-x64 bin/slang.dll bin/slang-glsl-module.dll bin/slangd.exe
 target_build "$WIN32_ARM64_ZIP" win32-arm64 bin/slang.dll bin/slang-glsl-module.dll bin/slangd.exe
 target_build "$LINUX_X64_ZIP" linux-x64 lib/libslang.so lib/libslang-glsl-module.so bin/slangd
@@ -35,4 +42,18 @@ target_build "$LINUX_ARM64_ZIP" linux-arm64 lib/libslang.so lib/libslang-glsl-mo
 target_build "$DARWIN_X64_ZIP" darwin-x64 lib/libslang.dylib lib/libslang-glsl-module.dylib bin/slangd
 target_build "$DARWIN_ARM64_ZIP" darwin-arm64 lib/libslang.dylib lib/libslang-glsl-module.dylib bin/slangd
 
+echo "restoring .vscodeignore after native build ($TARGET)"
+mv .vscodeignore.bak .vscodeignore
+
+# For web build, exclude native/server files
+echo "updating .vscodeignore for web build"
+cp .vscodeignore .vscodeignore.bak
+# Exclude native/server files for web build
+echo 'media/*.node.js' >> .vscodeignore
+echo 'client/dist/nativeClientMain.js' >> .vscodeignore
+echo 'server/dist/nativeServerMain.js' >> .vscodeignore
+
 vsce package --target "web"
+
+echo "restoring .vscodeignore after web build"
+mv .vscodeignore.bak .vscodeignore
