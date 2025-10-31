5deafff6b50bc5f356946581fde72b07735b6641
James Helferty (NVIDIA) <jhelferty@nvidia.com>
2025-10-31 19:12:22 -0400

    Update slang-vscode-extension for library rename and versioning (#54)
    * Update for library rename and versioning
    
    For shader-slang/slang#8828
    
    Handle new filename patterns introduced by shader-slang/slang#4016
    
    * Include slang-compiler.dll
    
    Forgot to add this for windows platform
    
    * Enable build of vsix for PRs

--- build_scripts/build-package.sh
+++ build_scripts/build-package.sh
@@ -6,18 +6,28 @@ target_build() {
   TEMP_DIR="$(mktemp -d)"
   ZIP="$1"
   TARGET="$2"
-  TEMP_LIBRARY="$3"
-  TEMP_EXECUTABLE="$4"
-  TEMP_LIBRARY_2="$5"
+  TEMP_EXECUTABLE="$3"
+  shift 3
+  # Remaining arguments are library patterns (may include wildcards)
+  TEMP_LIBRARY_PATTERNS=("$@")
 
   echo "extracting $ZIP"
   unzip -n "$ZIP" -d "$TEMP_DIR"
 
   echo "installing binaries for $TARGET"
   mkdir -p "./server/bin/$TARGET"
-  cp "$TEMP_DIR/$TEMP_LIBRARY" ./server/bin/"$TARGET"/
-  cp "$TEMP_DIR/$TEMP_LIBRARY_2" ./server/bin/"$TARGET"/
   cp "$TEMP_DIR/$TEMP_EXECUTABLE" ./server/bin/"$TARGET"/
+
+  # Copy libraries, expanding wildcards with temp directory prefix
+  for pattern in "${TEMP_LIBRARY_PATTERNS[@]}"; do
+    # Expand the pattern with TEMP_DIR prefix (unquoted to allow glob expansion)
+    for file in $TEMP_DIR/$pattern; do
+      if [ -e "$file" ]; then
+        cp "$file" ./server/bin/"$TARGET"/
+      fi
+    done
+  done
+
   chmod +x ./server/bin/"$TARGET"/*
 
   echo "building for $TARGET"
@@ -35,12 +45,12 @@ echo '' >> .vscodeignore
 echo 'media/*.worker.js' >> .vscodeignore
 echo 'server/dist/browserServerMain.js' >> .vscodeignore
 
-target_build "$WIN32_X64_ZIP" win32-x64 bin/slang.dll bin/slang-glsl-module.dll bin/slangd.exe
-target_build "$WIN32_ARM64_ZIP" win32-arm64 bin/slang.dll bin/slang-glsl-module.dll bin/slangd.exe
-target_build "$LINUX_X64_ZIP" linux-x64 lib/libslang.so lib/libslang-glsl-module.so bin/slangd
-target_build "$LINUX_ARM64_ZIP" linux-arm64 lib/libslang.so lib/libslang-glsl-module.so bin/slangd
-target_build "$DARWIN_X64_ZIP" darwin-x64 lib/libslang.dylib lib/libslang-glsl-module.dylib bin/slangd
-target_build "$DARWIN_ARM64_ZIP" darwin-arm64 lib/libslang.dylib lib/libslang-glsl-module.dylib bin/slangd
+target_build "$WIN32_X64_ZIP" win32-x64 bin/slangd.exe bin/slang.dll bin/slang-compiler.dll bin/slang-glsl-module.dll
+target_build "$WIN32_ARM64_ZIP" win32-arm64 bin/slangd.exe bin/slang.dll bin/slang-compiler.dll bin/slang-glsl-module.dll
+target_build "$LINUX_X64_ZIP" linux-x64 bin/slangd lib/libslang.so lib/libslang-compiler.so\* lib/libslang-glsl-module\*.so
+target_build "$LINUX_ARM64_ZIP" linux-arm64 bin/slangd lib/libslang.so lib/libslang-compiler.so\* lib/libslang-glsl-module\*.so
+target_build "$DARWIN_X64_ZIP" darwin-x64 bin/slangd lib/libslang.dylib lib/libslang-compiler\*.dylib lib/libslang-glsl-module\*.dylib
+target_build "$DARWIN_ARM64_ZIP" darwin-arm64 bin/slangd lib/libslang.dylib lib/libslang-compiler\*.dylib lib/libslang-glsl-module\*.dylib
 
 echo "restoring .vscodeignore after native build ($TARGET)"
 mv .vscodeignore.bak .vscodeignore
